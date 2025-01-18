82c5fa59e188c2f2ed67d73bee8f13e1bd51231b
杨风鸣 <yangfm@myy.ltd>
2025-01-18 20:29:32 +0800

    feat: 点积

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use concurrency::Matrix;
+
+
 fn main() {
+    let a= Matrix::new([1, 2, 3, 4, 5, 6], 2, 3);
+    let b= Matrix::new([1, 2, 3, 4, 5, 6], 3, 2);
+    // let c = multiply(&a, &b);
+    println!("a * b: {}", a*b);
     println!("Hello, world!");
 }
---
+++ src/metrics.rs
@@ -0,0 +1,6 @@
+use std::collections::HashMap;
+
+
+pub struct Metrics {
+    data: HashMap<String, f64>,
+}
